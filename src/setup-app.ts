4b274b7ed5f8020916b726c1802096afb73dbfa4
EllyVeny <nastyrakova04@gmail.com>
2026-04-16 21:39:53 +0700

    Change path for testing

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -5,7 +5,7 @@ import {testingRouter} from "./testing/routers/testing.roter";
 export const setupApp = (app: Express) => {
     app.use(express.json());
     app.use("/videos", videoRouter);
-    app.use("/testing", testingRouter);
+    app.use("/videos/testing", testingRouter);
 
 
     app.get("/", (req: Request, res: Response) => {
