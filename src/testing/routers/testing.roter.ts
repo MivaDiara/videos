d7711ac07f679e3846492098504de2f1c92ca1a0
EllyVeny <nastyrakova04@gmail.com>
2026-04-16 21:00:27 +0700

    Fixed router delete all-data

--- src/testing/routers/testing.roter.ts
+++ src/testing/routers/testing.roter.ts
@@ -5,7 +5,7 @@ import {db} from "../../db/db";
 export const testingRouter = Router();
 
 testingRouter
-.delete("/all_data", (req: Request, res: Response) => {
+.delete("/all-data", (req: Request, res: Response) => {
     db.videos = [];
     res.status(204).send(db.videos);
 })
